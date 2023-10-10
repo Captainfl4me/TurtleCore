dd992a77847c39dd48495208f9385583474697eb
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2023-10-10 23:10:58 +0200

    add: copy old instructions with new Instruction class

--- Python_logic_generator/pla_generator.py
+++ Python_logic_generator/pla_generator.py
@@ -81,8 +81,8 @@ class Instruction:
             self.append_hex_to_cycle(3, PCL_PCL|PCH_PCH)
         elif self.__addressing_mode == AdressModesList.ABS:
             self.append_hex_to_cycle(2, PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC)
-            self.append_hex_to_cycle(3, DL_DB|DB_ADD|O_ADD|PCL_PCL|PCH_PCH|PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|SUMS)
-            self.append_hex_to_cycle(4, DL_ADH|ADH_ABH|ADD_ADL|ADL_ABL|I_PC)
+            self.append_hex_to_cycle(3, DL_DB|DB_ADD|O_ADD|PCL_PCL|PCH_PCH|PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|SUMS|I_PC)
+            self.append_hex_to_cycle(4, DL_ADH|ADH_ABH|ADD_ADL|ADL_ABL|PCL_PCL|PCH_PCH)
         elif self.__addressing_mode == AdressModesList.ABSX:
             raise NotImplementedError('Absolute, X-indexed addressing mode is not implemented yet!')
         elif self.__addressing_mode == AdressModesList.ABSY:
@@ -127,6 +127,28 @@ class Instruction:
         else:
             self.__cycles[cycle] = self.__cycles[cycle] | value
 
+    def __str__(self) -> str:
+        instr_str = ''
+        if len(self.__cycles) == 0:
+            raise ValueError('Instruction has no cycles!')
+
+        # add reset cycle if instruction has less than 16 cycles
+        cycle_with_reset = self.__cycles
+        if len(self.__cycles) < 16:
+            cycle_with_reset.append(RST_CYCLE)
+
+        for microcounter, cycle in enumerate(cycle_with_reset):
+            instr_str += f'{Instruction.createAddress(self.opcode, microcounter):013b} {cycle:063b}\n'
+        return instr_str
+
+    @staticmethod
+    def createAddress(opcode, microCounter):
+        return opcode << 5 | 0 | microCounter
+
+    @staticmethod
+    def createOpCodeFromabc(a, b, c):
+        return hex(a << 5 | b << 2 | c )
+
 def generate_instruction_docs(instructions: list[Instruction]) -> str:
     """
     This function generates markdown documentation table of all instructions.
@@ -136,15 +158,26 @@ def generate_instruction_docs(instructions: list[Instruction]) -> str:
         return_string += f"${instruction.opcode:02x} | {instruction.name} {instruction.addressing_mode.value.short_name} | {len(instruction.cycles)}\n"
     return return_string
 
+def write_decode_PLA(instructions: list[Instruction]) -> None:
+    file = open('./PLAs/DecodePLA.txt', 'w', encoding='utf-8')
+    # write first line of file
+    file.write('# Logisim PLA program table\n')
+    file.write(f"xxxxxxxxx0000 {(ADH_ABH|ADL_ABL|I_PC|PCL_ADL|PCH_ADH):063b}\n")
+    file.write(f"xxxxxxxxx0001 {(PCL_PCL|PCH_PCH):063b}\n")
+    for instruction in instructions:
+        file.write(str(instruction))
+    file.close()
+    
 def main():
     instructions: list[Instruction] = []
 
+    # BRK impl
     brk = Instruction('BRK', 0x00, AdressModesList.IMP)
     brk.set_cycle(2, DBx_ADD|O_ADD|I_ADDC|SUMS|S_ADL|ADL_ABL)
     brk.set_cycle(3, ADD_SB06|ADD_SB7|SB_ADH|ADH_ABH|PCH_DB)
     brk.append_hex_to_cycle(4, RW|DB_ADD|S_SB|SB_ADD|SUMS)
     brk.append_hex_to_cycle(5, ADD_SB06|ADD_SB7|SB_S|ADD_ADL|ADL_ABL|PCL_DB)
-    brk.append_hex_to_cycle(6, RW|DB_ADD|S_SB|SB_ADD|SUMS|SB_X)
+    brk.append_hex_to_cycle(6, RW|DB_ADD|S_SB|SB_ADD|SUMS)
     brk.append_hex_to_cycle(7, ADD_SB06|ADD_SB7|SB_S|ADD_ADL|ADL_ABL|P_DB)
     brk.append_hex_to_cycle(8, RW|DB_ADD|S_SB|SB_ADD|SUMS)
     brk.append_hex_to_cycle(9, ADD_SB06|ADD_SB7|SB_S)
@@ -152,36 +185,70 @@ def main():
     brk.append_hex_to_cycle(11, DL_ADH|ADH_PCH)
     instructions.append(brk)
 
+    # LDY immediate
+    ldy_imm = Instruction('LDY', 0xa0, AdressModesList.IMM)
+    ldy_imm.append_hex_to_cycle(3, DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N)
+    ldy_imm.append_hex_to_cycle(4, ADD_SB06|ADD_SB7|SB_Y)
+    instructions.append(ldy_imm)
+
+    # LDA zeropage
+    lda_zpg = Instruction('LDA', 0xa5, AdressModesList.ZPG)
+    lda_zpg.append_hex_to_cycle(4, DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N)
+    lda_zpg.append_hex_to_cycle(5, ADD_SB06|ADD_SB7|SB_AC)
+    instructions.append(lda_zpg)
+
+    # LDA immediate
+    lda_imm = Instruction('LDA', 0xa9, AdressModesList.IMM)
+    lda_imm.append_hex_to_cycle(3, DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N)
+    lda_imm.append_hex_to_cycle(4, ADD_SB06|ADD_SB7|SB_AC)
+    instructions.append(lda_imm)
+
+    # LDA absolute
+    lda_abs = Instruction('LDA', 0xad, AdressModesList.ABS)
+    lda_abs.append_hex_to_cycle(5, DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N)
+    lda_abs.append_hex_to_cycle(6, ADD_SB06|ADD_SB7|SB_AC)
+    instructions.append(lda_abs)    
+
+
+    # STA absolute
+    sta_abs = Instruction('STA', 0x8d, AdressModesList.ABS)
+    sta_abs.append_hex_to_cycle(4, AC_DB)
+    sta_abs.append_hex_to_cycle(5, RW)
+
+    # TSX impl
+    tsx_impl = Instruction('TSX', 0xba, AdressModesList.IMP)
+    tsx_impl.append_hex_to_cycle(2, X_SB|SB_S|SB_DB|DBZ_Z|DB7_N)
+    instructions.append(tsx_impl)
+    # TXS impl
+    txs_impl = Instruction('TXS', 0x9a, AdressModesList.IMP)
+    txs_impl.append_hex_to_cycle(2, X_SB|SB_S)
+    instructions.append(txs_impl)
+
+    # TXA impl
+    txa_impl = Instruction('TXA', 0x8a, AdressModesList.IMP)
+    txa_impl.append_hex_to_cycle(2, X_SB|SB_AC|SB_DB|DBZ_Z|DB7_N)
+    instructions.append(txa_impl)
+    # TAX impl
+    tax_impl = Instruction('TAX', 0xaa, AdressModesList.IMP)
+    tax_impl.append_hex_to_cycle(2, AC_SB|SB_X|SB_DB|DBZ_Z|DB7_N)
+    instructions.append(tax_impl)
+
+    # TYA impl
+    tya_impl = Instruction('TYA', 0x98, AdressModesList.IMP)
+    tya_impl.append_hex_to_cycle(2, Y_SB|SB_AC|SB_DB|DBZ_Z|DB7_N)
+    instructions.append(tya_impl)
+    # TAY impl
+    tay_impl = Instruction('TAY', 0xa8, AdressModesList.IMP)
+    tay_impl.append_hex_to_cycle(2, AC_SB|SB_Y|SB_DB|DBZ_Z|DB7_N)
+    instructions.append(tay_impl)
+
     print("------ DOC INSTRCUTION TABLE -------")
     print(generate_instruction_docs(instructions))
+    print("------ GENERATE PLA -------")
+    write_decode_PLA(instructions)
     return
 
 """
-def convertBinToStr(binary, fill = PLAOUT_LEN):
-    return bin(binary)[2:].zfill(fill)
-
-def formatPLALine(addr, out):
-    return convertBinToStr(addr, 13) + ' ' + convertBinToStr(out) + '\n'
-
-def createOpCodeFromabc(a, b, c):
-    return hex(a << 5 | b << 2 | c )
-
-def createAddress(opcode, microCounter):
-    return opcode << 5 | 0 | microCounter
-
-# intruction OPCODE
-BRK = [ 0x00 ]
-LDY = [ 0xa0, 0xa4,   -1, 0xac,   -1, 0xb4,   -1, 0xbc ]
-LDA = [ 0xa1, 0xa5, 0xa9, 0xad, 0xb1, 0xb5, 0xb9, 0xbd ]
-TYA = [ 0x98 ]
-TXA = [ 0x8a ]
-TAY = [ 0xa8 ]
-TAX = [ 0xaa ]
-TSX = [ 0xba ]
-TXS = [ 0x9a ]
-STA = [ 0x81, 0x85, 0x8d, 0x91, 0x95, 0x99, 0x9d ]
-
-
 # Interrupt vectors control
 file = open('IRQPLA.txt', 'w')
 # write first line of file
@@ -191,53 +258,6 @@ file.write(convertBinToStr(BRK[0], 8) + ' ' + convertBinToStr(0b1, 2) + '\n')
 
 file.close()
 
-file = open('DecodePLA.txt', 'w')
-# write first line of file
-file.write('# Logisim PLA program table\n')
-
-# Share fetch opcode
-file.write("xxxxxxxxx0000" + ' ' + convertBinToStr(ADH_ABH|ADL_ABL|I_PC|PCL_ADL|PCH_ADH) + '\n')
-file.write("xxxxxxxxx0001" + ' ' + convertBinToStr(PCL_PCL|PCH_PCH) + '\n')
-
-# BRK impl
-file.write(formatPLALine(createAddress(BRK[0], 2), DBx_ADD|O_ADD|I_ADDC|SUMS|S_ADL|ADL_ABL))
-file.write(formatPLALine(createAddress(BRK[0], 3), ADD_SB06|ADD_SB7|SB_ADH|ADH_ABH|PCH_DB))
-file.write(formatPLALine(createAddress(BRK[0], 4), RW|DB_ADD|S_SB|SB_ADD|SUMS))
-file.write(formatPLALine(createAddress(BRK[0], 5), ADD_SB06|ADD_SB7|SB_S|ADD_ADL|ADL_ABL|PCL_DB))
-file.write(formatPLALine(createAddress(BRK[0], 6), RW|DB_ADD|S_SB|SB_ADD|SUMS|SB_X))
-file.write(formatPLALine(createAddress(BRK[0], 7), ADD_SB06|ADD_SB7|SB_S|ADD_ADL|ADL_ABL|P_DB))
-file.write(formatPLALine(createAddress(BRK[0], 8), RW|DB_ADD|S_SB|SB_ADD|SUMS))
-file.write(formatPLALine(createAddress(BRK[0], 9), ADD_SB06|ADD_SB7|SB_S))
-file.write(formatPLALine(createAddress(BRK[0], 10), DL_ADL|ADL_PCL))
-file.write(formatPLALine(createAddress(BRK[0], 11), DL_ADH|ADH_PCH))
-file.write(formatPLALine(createAddress(BRK[0], 12), RST_CYCLE))
-
-# LDY immediate
-file.write(formatPLALine(createAddress(LDY[0], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
-file.write(formatPLALine(createAddress(LDY[0], 3), PCL_PCL|PCH_PCH|DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N))
-file.write(formatPLALine(createAddress(LDY[0], 4), ADD_SB06|ADD_SB7|SB_Y))
-file.write(formatPLALine(createAddress(LDY[0], 5), RST_CYCLE))
-
-# LDA zeropage
-file.write(formatPLALine(createAddress(LDA[1], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
-file.write(formatPLALine(createAddress(LDA[1], 3), PCL_PCL|PCH_PCH|DL_ADL|ADL_ABL|O_ADH0|O_ADH17|ADH_ABH));
-file.write(formatPLALine(createAddress(LDA[1], 4), DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N|SB_X))
-file.write(formatPLALine(createAddress(LDA[1], 5), ADD_SB06|ADD_SB7|SB_AC))
-file.write(formatPLALine(createAddress(LDA[1], 6), RST_CYCLE))
-
-# LDA immediate
-file.write(formatPLALine(createAddress(LDA[2], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
-file.write(formatPLALine(createAddress(LDA[2], 3), PCL_PCL|PCH_PCH|DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N|SB_X))
-file.write(formatPLALine(createAddress(LDA[2], 4), ADD_SB06|ADD_SB7|SB_AC))
-file.write(formatPLALine(createAddress(LDA[2], 5), RST_CYCLE))
-
-# LDA absolute
-file.write(formatPLALine(createAddress(LDA[3], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
-file.write(formatPLALine(createAddress(LDA[3], 3), DL_DB|DB_ADD|O_ADD|PCL_PCL|PCH_PCH|PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|SUMS))
-file.write(formatPLALine(createAddress(LDA[3], 4), DL_ADH|ADH_ABH|ADD_ADL|ADL_ABL|I_PC))
-file.write(formatPLALine(createAddress(LDA[3], 5), PCL_PCL|PCH_PCH|DL_DB|DB_ADD|O_ADD|SUMS|DBZ_Z|DB7_N|SB_X))
-file.write(formatPLALine(createAddress(LDA[3], 6), ADD_SB06|ADD_SB7|SB_AC))
-file.write(formatPLALine(createAddress(LDA[3], 7), RST_CYCLE))
 
 # LDA absolute, Y
 file.write(formatPLALine(createAddress(LDA[6], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
@@ -257,27 +277,7 @@ file.write(formatPLALine(createAddress(LDA[6], 6), DL_DB|DB_ADD|O_ADD|SUMS))
 file.write(formatPLALine(createAddress(LDA[6], 7), ADD_SB06|ADD_SB7|DBZ_Z|DB7_N|SB_AC))
 file.write(formatPLALine(createAddress(LDA[6], 8), RST_CYCLE))
 
-# STA absolute
-file.write(formatPLALine(createAddress(STA[2], 2), PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|I_PC))
-file.write(formatPLALine(createAddress(STA[2], 3), DL_DB|DB_ADD|O_ADD|PCL_PCL|PCH_PCH|PCL_ADL|PCH_ADH|ADL_ABL|ADH_ABH|SUMS))
-file.write(formatPLALine(createAddress(STA[2], 4), DL_ADH|ADH_ABH|ADD_ADL|ADL_ABL|I_PC|AC_DB))
-file.write(formatPLALine(createAddress(STA[2], 5), PCL_PCL|PCH_PCH|RW))
-file.write(formatPLALine(createAddress(STA[6], 6), RST_CYCLE))
 
-# TSX impl
-file.write(formatPLALine(createAddress(TSX[0], 2), S_SB|SB_X|SB_DB|DBZ_Z|DB7_N))
-# TXS impl
-file.write(formatPLALine(createAddress(TXS[0], 2), X_SB|SB_S))
-
-# TXA impl
-file.write(formatPLALine(createAddress(TXA[0], 2), X_SB|SB_AC|SB_DB|DBZ_Z|DB7_N))
-# TAX impl
-file.write(formatPLALine(createAddress(TAX[0], 2), AC_SB|SB_X|SB_DB|DBZ_Z|DB7_N))
-
-# TYA impl
-file.write(formatPLALine(createAddress(TYA[0], 2), Y_SB|SB_AC|SB_DB|DBZ_Z|DB7_N))
-# TAY impl
-file.write(formatPLALine(createAddress(TAY[0], 2), AC_SB|SB_Y|SB_DB|DBZ_Z|DB7_N))
 
 # Close the file
 file.close()
